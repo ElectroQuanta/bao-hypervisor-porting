bbeadc833e6346166cc97f65b69569f6b5ea4a32
ElectroQuanta <id6892@alunos.uminho.pt>
2024-01-23 14:22:00 +0000

    UPDT: drivers path & plat description

--- src/platform/imx8mn-ddr3l-evk/imx8mn_ddr3l_evk_desc.c
+++ src/platform/imx8mn-ddr3l-evk/imx8mn_ddr3l_evk_desc.c
@@ -1,46 +1,46 @@
 /**
- * @file bb_ai64_desc.c
+ * @file imx8mn_ddr3l_evk_desc.c
  * @author Jose Pires
- * @date 2023-10-14
+ * @date 2024-01-23
  *
- * @brief Beaglebone AI-64 Platform description for the Bao hypervisor
+ * @brief i.MX 8M Nano UltraLite (DDR3L) EVK (imx8mn_ddr3l_evk) platform
+ * description for the Bao hypervisor
  *
- * See as references:
- * - Technical Reference Manual for the J721E DRA829/TDA4VM
- * Processors (available at
- * https://git.beagleboard.org/beagleboard/beaglebone-ai-64/-/tree/main/doc)
- * - Main device tree for the TI K3-J721E processor (available at
- * https://raw.githubusercontent.com/torvalds/linux/master/arch/arm64/boot/dts/ti/k3-j721e-main.dtsi)
- * - Device tree for the Beaglebone AI-64 (available at
- * https://raw.githubusercontent.com/torvalds/linux/master/arch/arm64/boot/dts/ti/k3-j721e-beagleboneai64.dts)
+ * References:
+ * - Technical Reference Manual for the i.MX 8M Nano
+ * (https://www.nxp.com/webapp/Download?colCode=IMX8MNRM)
+ * - Device trees: https://github.com/nxp-imx/uboot-imx/tree/lf_v2022.04/arch/arm/dts
+ *   - imx8mn-dd3rl-evk.dts
+ *   - imx8mn.dtsi
+ *   - imx8mn-evk.dtsi
  *
  * @license SPDX-License-Identifier: Apache-2.0
  *
  * @copyright Bao Project and Contributors. All rights reserved.
  */
 
-
 #include <platform.h>
 
 /**< RAM: imx8mn-dd3rl-evk.dts */
-//
 //	memory@40000000 {
 //		device_type = "memory";
 //		reg = <0x00 0x40000000 0x00 0x80000000>;
 //	};
 #define RAM1_ADDR 0x40000000
-#define RAM1_SIZE 0x80000000
+#define RAM1_SIZE_2G 0x80000000 /**< U-boot device tree */
+#define RAM1_SIZE_1G 0x40000000 /**< DDR3L Datasheet (1GB) */
 
-#define CPU_NUM 4 /**< Quad-core A52 processor */
+#define CPU_NUM 4 /**< Quad-core A53 processor */
 
-/**< imx-uboot/arch/arm/dts/imx8mn-evk.dts */
+/**< uboot-imx/arch/arm/dts/imx8mn-evk.dtsi */
 // / {
 // 	model = "NXP i.MX8MNano EVK board";
 // 	compatible = "fsl,imx8mn-evk", "fsl,imx8mn";
 // 
-// 	chosen {
-// 		bootargs = "console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200";
-// 	};
+//	chosen {
+//		bootargs = "console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200";
+//		stdout-path = &uart2;
+//	};
 // 
 // };
 /**< imx8mn.dtsi */
@@ -78,39 +78,27 @@
 
 
 struct platform platform = {
-    .cpu_num = CPU_NUM, /**< Dual-core A72 processor */
+    .cpu_num = CPU_NUM,
     .region_num = 1,
 
     .regions =  (struct mem_region[]) {
         // 3GB RAM
         {
             .base = RAM1_ADDR,
-            .size = RAM1_SIZE
+            .size = RAM1_SIZE_1GB
         }
         // Reserved memory
     },
 
     .console = {
-        .base = UART2_ADDR /**< serial1: (main.dtsi) */
+        .base = UART2_ADDR 
     },
 
     .arch = {
-        .clusters =  {
-            .num = 1,
-            .core_num = (size_t[]) {CPU_NUM}
-        },
-
-// @imx8mn.dtsi        
-//    gic: interrupt-controller@38800000 {
-//            compatible = "arm,gic-v3";
-//            reg = <0x38800000 0x10000>,
-//            <0x38880000 0xc0000>;
-//#interrupt-cells = <3>;
-//            interrupt-controller;
-//            interrupts = <GIC_PPI 9 IRQ_TYPE_LEVEL_HIGH>;
-//        };
-// 	
-// }
+        //.clusters =  {
+        //    .num = 1,
+        //    .core_num = (size_t[]) {CPU_NUM}
+        //},
         .gic = {
             .gicd_addr = GICD_ADDR,
             .gicr_addr = GICR_ADDR,
