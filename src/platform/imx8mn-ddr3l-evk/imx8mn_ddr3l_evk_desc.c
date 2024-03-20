726f286aacc5eb8ee5d2a46dbb23ac7b1cfcae2b
ElectroQuanta <id6892@alunos.uminho.pt>
2024-03-20 22:09:47 +0000

    UPDT: RAM addresses and sizes

--- src/platform/imx8mn-ddr3l-evk/imx8mn_ddr3l_evk_desc.c
+++ src/platform/imx8mn-ddr3l-evk/imx8mn_ddr3l_evk_desc.c
@@ -26,19 +26,24 @@
 //		device_type = "memory";
 //		reg = <0x00 0x40000000 0x00 0x80000000>;
 //	};
+/// RAM adddresses
 #define RAM1_ADDR_DTB       0x40000000UL
 #define RAM1_ADDR_UBOOT     0x40200000UL
 #define RAM1_ADDR_CUR       0x50000000UL
-//#define RAM1_ADDR RAM1_ADDR_UBOOT
-//#define RAM1_ADDR RAM1_ADDR_CUR
-#define RAM1_ADDR RAM1_ADDR_DTB
+
+/// RAM Sizes
 #define RAM1_SIZE_2GB       0x80000000UL /**< U-boot device tree */
 #define RAM1_SIZE_1GB       0x40000000UL /**< DDR3L Datasheet (1GB) */
 #define RAM1_SIZE_750MB     0x30000000UL /**< DDR3L Datasheet (1GB) */
-//#define RAM1_SIZE ( RAM1_SIZE_1GB - (RAM1_ADDR - RAM1_ADDR_DTB )) 
-//#define RAM1_SIZE 0x3FE00000 
-//#define RAM1_SIZE RAM1_SIZE_750MB 
-#define RAM1_SIZE RAM1_SIZE_2GB 
+
+/// RAM address and size calculation
+//#define RAM1_ADDR RAM1_ADDR_DTB
+//#define RAM1_ADDR RAM1_ADDR_UBOOT
+#define RAM1_ADDR RAM1_ADDR_CUR
+//#define RAM1_SIZE_ORIG RAM1_SIZE_2GB
+#define RAM1_SIZE_ORIG RAM1_SIZE_1GB
+
+#define RAM1_SIZE ( RAM1_SIZE_ORIG - (RAM1_ADDR - RAM1_ADDR_DTB )) 
 
 #define CPU_NUM 4 /**< Quad-core A53 processor */
 
